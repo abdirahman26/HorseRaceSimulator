958e1a899e40a7c189d6a724fffc5186e7d4b77c
Abdirahman Mohamed <ilymo1041@gmail.com>
2024-04-26 01:56:35 +0100

    printRace updated (One racetrack printed)

--- Part1/Race.java
+++ Part1/Race.java
@@ -169,6 +169,8 @@ public class Race
     private void printRace()
     {
         System.out.print('\u000C');  //clear the terminal window
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
         
         multiplePrint('=',raceLength+3); //top edge of track
         System.out.println();
